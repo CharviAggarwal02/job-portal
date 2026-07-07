9f801458f7d9e47398347d43243e2b2246e60213
Charvi Aggarwal <153435560+CharviAggarwal02@users.noreply.github.com>
2026-07-07 22:56:50 +0530

    Update app configuration to use environment variables
    Refactor app configuration to use environment variables for SECRET_KEY and DATABASE_URL.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -5,13 +6,25 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobportal.db'
+
+    app.config["SECRET_KEY"] = os.environ.get(
+        "SECRET_KEY",
+        "dev-secret-key"
+    )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///jobportal.db"
+    )
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = "main.login"
 
     from .models import User
 
@@ -22,10 +35,9 @@ def create_app():
     from .routes import main
     app.register_blueprint(main)
 
+    with app.app_context():
+        db.create_all()
+
     return app
-
-
-
-
 
 
